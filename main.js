f4c96a96e12f613e5cc599f52a8e757031bedd0f
Craig Monaghan <158125396+craigmonaghan@users.noreply.github.com>
2024-11-29 01:44:27 +0000

    Add basic calculator functionality with UI and styling
    - Implement calculator UI in `index.html` with buttons for digits, operations, and actions.
    - Add JavaScript functions in `main.js` for input handling, calculation, and clearing input.
    - Create `stylesheet.css` for styling the calculator layout and buttons.

--- main.js
+++ main.js
@@ -1,24 +1,37 @@
+const operators = ["+", "-", "*", "/"];
+let currentOp = "";
+const inputField = document.getElementById("calcInput");
 function appendToInput(value) {
   console.log("appendToInput", value);
-  const inputField = document.getElementById("calcInput");
+
+  if (operators.includes(currentOp) && operators.includes(value)) {
+    console.warn("Consecutive operators are not allowed.");
+    return;
+  }
+  if (operators.includes(inputField.value[inputField.value.legnth - 1])) {
+    inputField.value = inputField.value.slice(0, -1);
+    currentOp = "";
+  }
   inputField.value += value;
+  currentOp = value;
 }
 
 function calculateResult() {
-  const inputField = document.getElementById("calcInput");
   const result = math.evaluate(inputField.value);
   inputField.value = result;
+  if (result === Infinity) {
+    inputField.value = "Error.";
+  }
 }
 
 function clearInput() {
-  const inputField = document.getElementById("calcInput");
   inputField.value = "";
 }
 
 function clearEntry() {
-  const inputField = document.getElementById("calcInput");
   if (inputField && typeof inputField.value === "string") {
     inputField.value = inputField.value.slice(0, -1);
+    currentOp = " ";
   } else {
     console.log("Error.");
   }
